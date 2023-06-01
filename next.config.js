b62047289126b6aac8a98d083d1c158d9c5a66d2
Dredeco <andrefersouza@gmail.com>
2023-06-01 19:09:20 -0300

    Fix: Added the line 'compiler: styledcomponents = true' on next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    compiler: {
+        styledComponents: true,
+    },
     images: {
         domains: ['eldenring.fanapis.com'],
         unoptimized: true,
